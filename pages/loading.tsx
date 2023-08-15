f622341c895facd7da651bb31a624c9cb14f7b53
Tejas Agarwal <tejasagarwalbly@gmail.com>
2023-08-15 17:34:11 +0530

    added loading screen

---
+++ pages/loading.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+export default function Loading() {
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center ">
+        <div className="header-2">Loading...</div>
+      </div>
+    </>
+  );
+}
